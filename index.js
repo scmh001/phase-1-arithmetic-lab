1ff60cccd862b3e181d0df41e3c6e7b257baed18
Shukri Hussein <husseinshukri@outlook.com>
2024-03-06 21:52:05 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2; //multiply equals 62
+
+let random = 41
+Number.isInteger(random)  
+
+let num3 = 12
+let num4 = 3
+let mod = num3 / num4;
+
+let max = Math.max(3,20)
